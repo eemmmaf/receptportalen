e1de2716cae0c9e581f6b47371025c01b9ad7ef7
Emma Forslund <emfo2102@student.miun.se>
2022-06-20 14:02:56 +0200

    Added isset to decide what to show depending on logged in or not

--- includes/header.php
+++ includes/header.php
@@ -11,13 +11,18 @@ include('includes/config.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+    <script src="//cdn.ckeditor.com/4.17.2/standard/ckeditor.js"></script>
     <script src="https://kit.fontawesome.com/2090b52781.js" crossorigin="anonymous"></script>
     <title><?= $site_title . $divider . $page_title; ?></title>
 </head>
 
 <body>
     <header>
-        <h1><a class="logo" href="index.php">Receptportalen</i></a></h1>
+        <?php if (isset($_SESSION['email'])) { ?>
+            <h1><a class="logo" href="admin.php">Receptportalen</i></a></h1>
+        <?php } else { ?>
+            <h1><a class="logo" href="login.php">Receptportalen</i></a></h1>
+        <?php } ?>
         <nav class="desktop-nav">
             <ul>
                 <li><a href="about.php">Om sidan <i class="fa-solid fa-circle-info fa-2xs"></i></a></li>
@@ -30,8 +35,21 @@ include('includes/config.php');
             <button class="hamburger-icon" id="hamburger-icon"><i class="fas fa-bars"></i></button>
             <ul class="nav-ul" id="nav-ul">
                 <li><a href="about.php">Om sidan</a></li>
-                <li><a href="register.php">Skapa blogg</a></li>
-                <li><a href="login.php">Logga in <i class="fa-solid fa-right-to-bracket fa-2xs"></i></a></li>
+                <?php
+                if (!isset($_SESSION['email'])) { ?>
+                    <li><a href="register.php">Skapa blogg</a></li>
+                    <li><a href="login.php">Logga in <i class="fa-solid fa-right-to-bracket fa-2xs"></i></a></li>
+                <?php
+                } else {
+                ?>
+                    <li><a href="admin.php">Mina sidor</a></li>
+                    <li><a href="posts.php">Alla blogginlägg</a></li>
+                    <li><a href="create.php">Skapa nytt inlägg</li>
+                    <li><a href="logout.php">Logga ut <i class="fa-solid fa-right-to-bracket fa-2xs"></i></a></li>
+                <?php
+                }
+                ?>
+
             </ul>
         </nav>
         <!--Searchbox-->
